9fbf44847bcbcd867a5ea6486219e740408a0464
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2018-03-15 17:19:18 +0100

    Configure ioc

--- solutions/FaceLogin/src/face_recognition/config.ts
+++
@@ -1,5 +0,0 @@
-export const AzureConfiguration = {
-    apiKey: '68792d7388b247c59dbfbaabad08a0ea',
-    endpoint: 'westcentralus.api.cognitive.microsoft.com',
-//    endpoint: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0',
-};
