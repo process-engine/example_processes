0693203824a084c987145423f81e25d90145b933
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2018-03-15 17:34:28 +0100

    Add project files

---
+++ solutions/FaceLogin/bin/start.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const bootstrapperIocModule = require('@essential-projects/bootstrapper/ioc_module');
+const bootstrapperNodeIocModule = require('@essential-projects/bootstrapper_node/ioc_module');
+const InvocationContainer = require('addict-ioc').InvocationContainer;
+const logger = require('loggerhythm').Logger.createLogger('bootstrapper');
+
+const iocModuleNames = [
+  '..',
+  '@essential-projects/services',
+  '@essential-projects/bootstrapper',
+  '@essential-projects/bootstrapper_node',
+  '@essential-projects/caching',
+  '@essential-projects/core',
+  '@essential-projects/data_model',
+  '@essential-projects/data_model_contracts',
+  '@essential-projects/datasource_adapter_base',
+  '@essential-projects/datasource_adapter_postgres',
+  '@essential-projects/datastore',
+  '@essential-projects/datastore_http',
+  '@essential-projects/datastore_messagebus',
+  '@essential-projects/event_aggregator',
+  '@essential-projects/feature',
+  '@essential-projects/http_extension',
+  '@essential-projects/iam',
+  '@essential-projects/iam_http',
+  '@essential-projects/invocation',
+  '@essential-projects/messagebus',
+  '@essential-projects/messagebus_http',
+  '@essential-projects/messagebus_adapter_faye',
+  '@essential-projects/metadata',
+  '@essential-projects/pki_service',
+  '@process-engine/consumer_api',
+  '@process-engine/process_engine',
+  '@process-engine/process_engine_http',
+  '@process-engine/process_repository',
+  '@essential-projects/security_service',
+  '@essential-projects/routing',
+  '@essential-projects/timing',
+  '@essential-projects/validation',
+];
+
+const iocModules = iocModuleNames.map((moduleName) => {
+  return require(`${moduleName}/ioc_module`);
+});
+
+async function start() {
+
+  const container = new InvocationContainer({
+    defaults: {
+      conventionCalls: ['initialize'],
+    },
+  });
+
+  for (const iocModule of iocModules) {
+    iocModule.registerInContainer(container);
+  }
+
+  container.validateDependencies();
+
+  try {
+    const bootstrapper = await container.resolveAsync('AppBootstrapper');
+    await bootstrapper.start();
+
+    logger.info('Bootstrapper started successfully.');
+  } catch(error) {
+    logger.error('Bootstrapper failed to start.', error);
+  }
+
+  try {
+    const faceRecognitionServer = await container.resolveAsync('FaceRecognitionServer');
+    faceRecognitionServer.start();
+  } catch(error) {
+    logger.error('Error.', error);
+  }
+}
+
+start();
