09ad4ea311b190102fd3720c1a6b63c051f548bd
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2018-03-22 11:21:21 +0100

    Clean up; Add elizabot

---
+++ solutions/Therapist/ioc_module.js
@@ -0,0 +1,20 @@
+
+'use strict';
+const Therapist = require('elizabot');
+
+const entityDiscoveryTag = require('@essential-projects/core_contracts').EntityDiscoveryTag;
+
+const fs = require('fs');
+const path = require('path');
+const therapistBpmn = fs.readFileSync(path.join('bpmn', 'therapist.bpmn'), 'utf8');
+
+function registerInContainer(container) {
+
+  container.registerObject('therapistProcess', therapistBpmn)
+    .setTag('bpmn_process', 'internal')
+
+  const therapistKey = 'Therapist';
+  container.register(therapistKey, Therapist);
+}
+
+module.exports.registerInContainer = registerInContainer;
