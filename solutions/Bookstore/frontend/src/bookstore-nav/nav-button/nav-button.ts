b932c0d6ee1eff8e0c28d8b458e12f4e51bee7a5
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2018-04-30 10:59:11 +0200

    Add dropdown

--- solutions/Bookstore/frontend/src/bookstore-nav/nav-button/nav-button.ts
+++ solutions/Bookstore/frontend/src/bookstore-nav/nav-button/nav-button.ts
@@ -1,7 +1,77 @@
 import {bindable} from 'aurelia-framework';
 
 export class NavButton {
-  @bindable public title: string;
 
+  @bindable public title: string;
+  @bindable public toggleElementId: string;
+  @bindable public show: boolean = false;
+
+
+  private toggleElement: HTMLElement;
+
+
+
+  public attached() {
+
+    if (this.toggleElementId === undefined || this.toggleElementId === '') {
+      return;
+    }
+
+    const toggleElement: HTMLElement | null = document.getElementById(this.toggleElementId);
+    this.toggleElement = (toggleElement !== null) ? toggleElement : undefined;
+
+    if (this.show === true) {
+      this.showElement();
+      return;
+    }
+    this.hideElement();
+  }
+
+  private hideElement(): void {
+    console.log('hides')
+    this.toggleElement.style.display = 'none';
+  }
+
+  private showElement(): void {
+
+
+    this.toggleElement.style.display = 'block';
+    setTimeout(() => {
+      const eventListener = (event: MouseEvent) => {
+        // if event.target is child of toggleelement
+        console.log('fires already')
+        this.toggle();
+        document.removeEventListener('click', eventListener);
+      };
+
+      document.addEventListener('click', eventListener);
+
+    }, 100);
+  }
+
+
+  public showChanged(newValue: boolean): void {
+
+    if (this.toggleElement === undefined) {
+      return;
+    }
+
+    const toggleElement: HTMLElement = document.getElementById(this.toggleElementId);
+
+    if (newValue === true) {
+
+      this.showElement();
+
+
+      return;
+    }
+
+    this.hideElement();
+  }
+
+  public toggle(): void {
+
+    this.show = !this.show;
+  }
 
 }
