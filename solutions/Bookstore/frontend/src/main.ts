221d2e83ddba11f54d546fea4d24fe8e472e8aef
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2018-05-14 10:47:30 +0200

    Add aurelia dropdown plugin

--- solutions/Bookstore/frontend/src/main.ts
+++ solutions/Bookstore/frontend/src/main.ts
@@ -1,10 +1,12 @@
-import {Aurelia} from 'aurelia-framework'
+import {Aurelia, PLATFORM} from 'aurelia-framework'
 import environment from './environment';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources');
+
+  aurelia.use.plugin('aurelia-plugins-dropdown');
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
