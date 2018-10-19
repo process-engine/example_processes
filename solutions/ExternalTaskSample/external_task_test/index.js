5bd5d9878f2eebeb4a70f2a04c242dbb75d9cc32
Robin <robin.lenz@5minds.de>
2018-10-20 00:44:40 +0200

    :sparkles: add ExternalTask sample

---
+++ solutions/ExternalTaskSample/external_task_test/index.js
@@ -0,0 +1,43 @@
+const { HttpClient } = require('@essential-projects/http');
+const {
+    ExternalTaskApiClientService,
+    ExternalTaskApiExternalAccessor,
+    ExternalTaskWorker,
+} = require('@process-engine/external_task_api_client');
+const {
+    ExternalTaskFinished,
+} = require('@process-engine/external_task_api_contracts');
+
+const httpClient = new HttpClient();
+httpClient.config = {
+    url: 'http://localhost:8000',
+}
+
+const externalAccessor = new ExternalTaskApiExternalAccessor(httpClient);
+const externalTaskAPIService = new ExternalTaskApiClientService(externalAccessor);
+const externalTaskWorker = new ExternalTaskWorker(externalTaskAPIService);
+
+const identity = {
+    token: 'ZHVtbXlfdG9rZW4=',
+};
+const topicName = 'TestTopic';
+const maxNumberOfTasksToGet = 10;
+const longPollingTimeoutInMs = 10000;
+
+async function main() {
+
+    const handleTask = async (externalTask) => {
+        console.log(externalTask);
+        return new ExternalTaskFinished(externalTask.id, { prop: 'Fertig' });
+    };
+
+    externalTaskWorker.waitForAndHandle(
+        identity,
+        topicName,
+        maxNumberOfTasksToGet,
+        longPollingTimeoutInMs,
+        handleTask
+    );
+}
+
+main();
