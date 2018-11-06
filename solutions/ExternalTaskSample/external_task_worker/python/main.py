3a48f241a811050228d82297eb231f71dbe68bb9
Christoph <christoph.gnip@gmail.com>
2018-11-06 14:10:46 +0100

    :recycle: Improve import Statements

--- solutions/ExternalTaskSample/external_task_worker/python/main.py
+++ solutions/ExternalTaskSample/external_task_worker/python/main.py
@@ -1,23 +1,24 @@
 import asyncio
 import json
-import external_task_api_client.external_task_api_client_service as etacs
-import external_task_api_client.external_task_finished as etf
-import external_task_api_client.external_task_worker as etw
+
+from external_task_api_client.external_task_api_client_service import ExternalTaskApiClientService
+from external_task_api_client.external_task_finished import ExternalTaskFinished
+from external_task_api_client.external_task_worker import ExternalTaskWorker
 
 
 async def handle(externalTask):
     print(externalTask)
-    return etf.ExternalTaskFinished(externalTask["id"], {"testprop": "Hallo"})
+    return ExternalTaskFinished(externalTask["id"], {"testprop": "Hallo"})
 
 
 async def main():
     identity = {"token": "ZHVtbXlfdG9rZW4="}
     topicName = "TestTopic"
 
-    externalTaskApi = etacs.ExternalTaskApiClientService(
+    externalTaskApi = ExternalTaskApiClientService(
         "http://localhost:8000")
 
-    worker = etw.ExternalTaskWorker(externalTaskApi)
+    worker = ExternalTaskWorker(externalTaskApi)
 
     await worker.waitForHandle(identity, topicName, 10, 10000, handle)
 
