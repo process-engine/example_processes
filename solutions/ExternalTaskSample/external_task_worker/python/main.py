4d3b7a3fd030ceea4ea70e55917d0c1a7939989c
Christoph <christoph.gnip@gmail.com>
2018-11-06 17:43:48 +0100

    :recycle: Cleanup the main.py; Improve Sample Character

--- solutions/ExternalTaskSample/external_task_worker/python/main.py
+++ solutions/ExternalTaskSample/external_task_worker/python/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import asyncio
 import json
 
@@ -6,20 +9,31 @@ from external_task_api_client.external_task_finished import ExternalTaskFinished
 from external_task_api_client.external_task_worker import ExternalTaskWorker
 
 
-async def handle(externalTask):
-    print(externalTask)
-    return ExternalTaskFinished(externalTask["id"], {"testprop": "Hallo"})
+async def handle_external_task(task):
+    """This is used as callback, if an External Task can be processed.
+
+    :task: The External Task object we are supposed to process.
+    :returns: The ExternalTaskFinished as successful response.
+    """
+    print(task)
+
+    task_result = {"testprop": "Hallo"}
+
+    return ExternalTaskFinished(task["id"], task_result)
 
 
 async def main():
-    identity = {"token": "ZHVtbXlfdG9rZW4="}
-    topicName = "TestTopic"
+    """This is the main() of our external task sample."""
+    external_task_client = ExternalTaskApiClientService("http://localhost:8000")
 
-    externalTaskApi = ExternalTaskApiClientService(
-        "http://localhost:8000")
+    worker = ExternalTaskWorker(external_task_client)
 
-    worker = ExternalTaskWorker(externalTaskApi)
-
-    await worker.waitForHandle(identity, topicName, 10, 10000, handle)
+    await worker.waitForHandle(
+        identity={"token": "ZHVtbXlfdG9rZW4="},
+        topic="TestTopic",
+        maxTasks=10,
+        longPollingTimeout=10_000,
+        handleAction=handle_external_task
+    )
 
 asyncio.run(main())
